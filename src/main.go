bc8c2b9ee2bc74944049953e4b8a3615a620ba8a
fulecorafa <ra.pha@live.com>
2024-01-16 08:14:05 -0300

    fix: tabs

--- src/main.go
+++ src/main.go
@@ -14,23 +14,23 @@ import (
 )
 
 func setLogger() {
-    var handler slog.Handler
-    var options *slog.HandlerOptions
-    if isDebug {
-        lvl := new(slog.LevelVar)
-        lvl.Set(slog.LevelDebug)
-        options = &slog.HandlerOptions{
-            Level: lvl,
-        }
-    }
-    switch logFormat {
-    case "kv":
-        handler = slog.NewTextHandler(os.Stdout, options)
-    case "json":
-        handler = slog.NewJSONHandler(os.Stdout, options)
-    default:
-        panic(fmt.Sprintf("Unsupported log format: %s", logFormat))
-    }
+	var handler slog.Handler
+	var options *slog.HandlerOptions
+	if isDebug {
+		lvl := new(slog.LevelVar)
+		lvl.Set(slog.LevelDebug)
+		options = &slog.HandlerOptions{
+			Level: lvl,
+		}
+	}
+	switch logFormat {
+	case "kv":
+		handler = slog.NewTextHandler(os.Stdout, options)
+	case "json":
+		handler = slog.NewJSONHandler(os.Stdout, options)
+	default:
+		panic(fmt.Sprintf("Unsupported log format: %s", logFormat))
+	}
 
 	logger := slog.New(handler)
 
@@ -49,19 +49,20 @@ func init() {
 	flag.Var(&ports, "r", "Lists of ports redirecting to URLs in format 'port:url'")
 	flag.Var(&ports, "redirect", "Lists of ports redirecting to URLs in format 'port:url'")
 	flag.BoolVar(&isDebug, "debug", false, "Print debug logs")
-    flag.StringVar(&logFormat, "l", "kv", "Log format. One of: json; kv")
-    flag.StringVar(&logFormat, "logs", "kv", "Log format. One of: json; kv")
-    flag.StringVar(&configFilePath, "config", "example_config.json", "Path to config file. Defaults to '~/.config/customs/config.json'")
+	flag.StringVar(&logFormat, "l", "kv", "Log format. One of: json; kv")
+	flag.StringVar(&logFormat, "logs", "kv", "Log format. One of: json; kv")
+	flag.StringVar(&configFilePath, "config", "example_config.json", "Path to config file. Defaults to '~/.config/customs/config.json'")
 
-    LoadPlugins(configFilePath)
+	LoadPlugins(configFilePath)
+	// a comment
 
-    RegisterPluginFlags()
+	RegisterPluginFlags()
 }
 
 func main() {
 	flag.Parse()
 
-    setLogger()
+	setLogger()
 
 	if len(ports) == 0 {
 		fmt.Println("Nothing to do")
@@ -69,9 +70,9 @@ func main() {
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
-    var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
-    wg.Add(len(ports))
+	wg.Add(len(ports))
 	for _, r := range ports {
 		go Listen(ctx, &wg, r)
 	}
@@ -81,9 +82,9 @@ func main() {
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
 	defer close(signalChan)
 	go func() {
-        <-signalChan
-        cancel()
-        return
+		<-signalChan
+		cancel()
+		return
 	}()
-    wg.Wait()
+	wg.Wait()
 }
